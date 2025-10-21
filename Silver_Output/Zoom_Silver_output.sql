30db5dde2a894ab968a1361ff7301fb0c6c97290
Karthik Valluri <karthikvalluri@gmail.com>
2025-10-21 15:04:29 +0530

    Delete Silver_Output directory

--- Silver_Output/Zoom_Silver_output.sql
+++
@@ -1,8 +0,0 @@
-_____________________________________________
-## *Author*: AAVA
-## *Created on*: 2024-01-20
-## *Description*: Silver Layer Physical Data Model for Zoom Platform Analytics System with Snowflake DDL scripts
-## *Version*: 1
-## *Updated on*: 2024-01-20
-## *Changes*: Initial creation of Silver layer based on conceptual model and Bronze layer alignment
-## *Reason*: Transform Bronze layer raw data into business-ready
