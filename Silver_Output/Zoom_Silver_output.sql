f8d8b58edf51a277725b69fd4eef9a948a731b34
Karthik Valluri <karthikvalluri@gmail.com>
2025-10-21 14:57:56 +0530

    Add or update file: Zoom_Silver_output.sql

---
+++ Silver_Output/Zoom_Silver_output.sql
@@ -0,0 +1,8 @@
+_____________________________________________
+## *Author*: AAVA
+## *Created on*: 2024-01-20
+## *Description*: Silver Layer Physical Data Model for Zoom Platform Analytics System with Snowflake DDL scripts
+## *Version*: 1
+## *Updated on*: 2024-01-20
+## *Changes*: Initial creation of Silver layer based on conceptual model and Bronze layer alignment
+## *Reason*: Transform Bronze layer raw data into business-ready
