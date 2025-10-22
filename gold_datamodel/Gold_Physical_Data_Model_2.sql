94ed5dc42d2daf138ceaa1b6f640a1600a46554d
Karthik Valluri <karthikvalluri@gmail.com>
2025-10-22 18:23:34 +0530

    Add or update file: Gold_Physical_Data_Model_2.sql

---
+++ gold_datamodel/Gold_Physical_Data_Model_2.sql
@@ -0,0 +1,4 @@
+_____________________________________________
+## *Author*: AAVA
+## *Created on*: 
+## *Description*: Complete and accurate Gold Layer Physical Data Model for Zoom Platform Analytics System with comprehensive dimensional modeling
