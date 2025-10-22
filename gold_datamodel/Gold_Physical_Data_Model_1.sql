75db28270539c146eeefe2e54cb7a4bb7cc1566c
Karthik Valluri <karthikvalluri@gmail.com>
2025-10-22 18:00:58 +0530

    Delete gold_datamodel directory

--- gold_datamodel/Gold_Physical_Data_Model_1.sql
+++
@@ -1,4 +0,0 @@
-_____________________________________________
-## *Author*: AAVA
-## *Created on*: 
-## *Description*: Gold Layer Physical Data Model for Zoom Platform Analytics System with optimized fact tables
